0d8aa811dcfea5098677410e3908b67c985ae1aa
Allan Pure <allan@pure-illusion.com>
2025-07-05 14:32:38 +0200

    update: change API base URL from HTTP to HTTPS for improved security

--- portfolio_front/src/app/services/api.service.ts
+++ portfolio_front/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { catchError, Observable, throwError } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private static baseURL: string = 'http://api.allanperez.fr';
+  private static baseURL: string = 'https://api.allanperez.fr';
   constructor(private http: HttpClient) { }
 
   get<T>(url: string): Observable<T> {
